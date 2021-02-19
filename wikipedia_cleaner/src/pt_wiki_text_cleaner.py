b6737bfa0c28470e644aa88d7f5f13bbe673217d
Raj Nath Patel <patelrajnath@gmail.com>
2021-02-19 10:59:47 +0000

    Update the code.

--- wikipedia_cleaner/src/pt_wiki_text_cleaner.py
+++ wikipedia_cleaner/src/pt_wiki_text_cleaner.py
@@ -1,6 +1,7 @@
 import mwparserfromhell
 import re
 
+
 class PTwikitextCleaner(object):
     # Static definition of the filtered elements
 
@@ -10,30 +11,34 @@ class PTwikitextCleaner(object):
     # https://en.wikipedia.org/wiki/Template:Bulleted_list
     # https://en.wikipedia.org/wiki/Template:Flatlist
     # https://en.wikipedia.org/wiki/Template:Ordered_list
-    filter_template = ['unbulleted list','flatlist','hlist','bulleted list','ordered list','plainlist','defn','term', 'glossary','glossary end', 'outdent', 'outdent2','fake heading','col-begin','col-end', 'columns-list' ]
-    
+    filter_template = ['unbulleted list', 'flatlist', 'hlist', 'bulleted list', 'ordered list', 'plainlist', 'defn',
+                       'term', 'glossary', 'glossary end', 'outdent', 'outdent2', 'fake heading', 'col-begin',
+                       'col-end', 'columns-list']
+
     # detalhes: 
     # https://en.wikipedia.org/wiki/Help:Wikitext
     # https://en.wikipedia.org/wiki/Help:HTML_in_wikitext
-    filter_tags_and_templates = ['chem', 'chem2','graph','hiero','indicator','math','score','syntaxhighlight',
-                                 'source','code','templatedata','timeline','gallery','categorytree','table',
-                                 'tr','td','ul','li','ol','dl','dt','dd','ref']
-    
+    filter_tags_and_templates = ['chem', 'chem2', 'graph', 'hiero', 'indicator', 'math', 'score', 'syntaxhighlight',
+                                 'source', 'code', 'templatedata', 'timeline', 'gallery', 'categorytree', 'table',
+                                 'tr', 'td', 'ul', 'li', 'ol', 'dl', 'dt', 'dd', 'ref']
+
     # detalhes:
-    # https://pt.wikipedia.org/wiki/Ajuda:Guia_de_edi%C3%A7%C3%A3o/Como_usar_imagens#Sintaxe_para_multim%C3%ADdias
-    # Os prefixos "Imagem:", "Arquivo:", "File:" e "Image:" são reconhecidos pelo MediaWiki e apontam todos para o domínio "Ficheiro:"
+    # https://pt.wikipedia.org/wiki/Ajuda:Guia_de_edi%C3%A7%C3%A3o/
+    # Como_usar_imagens#Sintaxe_para_multim%C3%ADdias
+    # Os prefixos "Imagem:", "Arquivo:", "File:" e "Image:"
+    # são reconhecidos pelo MediaWiki e apontam todos para o domínio "Ficheiro:"
     filter_wikilinks = ['file:', 'imagem:', 'image:', 'arquivo:', 'media:']
-    
+
     # detalhes:
     # https://pt.wikipedia.org/wiki/Wikip%C3%A9dia:Livro_de_estilo
-    # Quando certas seções-padrão são usadas, estas devem ficar no final do artigo, respeitando a terminologia das seções, e uma das duas sequências seguinte:[2]
+    # Quando certas seções-padrão são usadas, estas devem ficar no final do artigo,
+    # respeitando a terminologia das seções, e uma das duas sequências seguinte:[2]
     # Notas, Referências, Ver também, Bibliografia, Ligações externas
     sections_headers = ['Referências', 'Ver também', 'Bibliografia', 'Ligações externas']
 
-
     def __init__(self, wikitext):
         self._wikitext = wikitext
-    
+
     def clean(self):
         wiki_text = self._sub_amp(self._wikitext)
         wiki_text = self._filter_identation_and_lists(wiki_text)
@@ -44,8 +49,8 @@ class PTwikitextCleaner(object):
 
         mw_parser = self._fix_categories(mw_parser)
         mw_parser = self._filter_specific_pt_elements(mw_parser)
-        mw_parser = self._filter_headings(mw_parser) # must be last otherwise the section will not be 
-        
+        mw_parser = self._filter_headings(mw_parser)  # must be last otherwise the section will not be
+
         plain_text = mw_parser.strip_code()
         plain_text = self._remove_tags(plain_text)
         plain_text = self._remove_non_identation_space(plain_text)
@@ -53,7 +58,7 @@ class PTwikitextCleaner(object):
         return plain_text
 
     def _fix_foreign(self, wikitext):
-        return re.sub(r'\{\{lang-[a-z]+\|([^}]*)\}\}', r"\1",wikitext)
+        return re.sub(r'\{\{lang-[a-z]+\|([^}]*)\}\}', r"\1", wikitext)
 
     def _sub_amp(self, wikitext):
         return wikitext.replace('&amp;', '&')
@@ -69,20 +74,20 @@ class PTwikitextCleaner(object):
         # ; -> description lists
         # * -> list
         # # -> ordered list
-        
-        #regex = r'^[\\t|\s]*[\*|\#|:|\;]+.+$'
+
+        # regex = r'^[\\t|\s]*[\*|\#|:|\;]+.+$'
         regex = r'^[\\t\s]*[\*\#:\;]+.+$'
         return re.sub(regex, '', wikitext, flags=re.MULTILINE)
 
     def _filter_images_wikilinks(self, wikitext):
         regex = r'^\[\[(imagem|image|arquivo|file|media).+\]\]$'
-        return re.sub(regex, '', wikitext, flags=re.MULTILINE|re.IGNORECASE)
+        return re.sub(regex, '', wikitext, flags=re.MULTILINE | re.IGNORECASE)
 
     def _fix_categories(self, prs):
         for l in prs.filter_wikilinks():
             if 'categoria:' in l.title.lower() or 'category:' in l.title.lower():
                 if not l.text or not l.text.strip(' ').strip('\t'):
-                    text = re.sub(r'^[^\:]+\:','', str(l.title))
+                    text = re.sub(r'^[^\:]+\:', '', str(l.title))
                     l.text = text
         return prs
 
@@ -97,10 +102,10 @@ class PTwikitextCleaner(object):
     def _filter_specific_pt_elements(self, prs):
 
         return self._filter_elements(prs,
-                                     tags = self.filter_tags_and_templates,
-                                     templates = self.filter_tags_and_templates + self.filter_template,
-                                     wikilinks = self.filter_wikilinks,
-                                     sections = self.sections_headers)
+                                     tags=self.filter_tags_and_templates,
+                                     templates=self.filter_tags_and_templates + self.filter_template,
+                                     wikilinks=self.filter_wikilinks,
+                                     sections=self.sections_headers)
 
     def _filter_elements(self, prs, tags, templates, wikilinks, sections):
         remove_elements = []
@@ -114,7 +119,7 @@ class PTwikitextCleaner(object):
         remove_elements = []
 
         # find nodes
-        for i,node in enumerate(list(prs.nodes)):
+        for i, node in enumerate(list(prs.nodes)):
             # filter tags
             if type(node) == mwparserfromhell.nodes.Tag:
                 if node.tag.lower() in tags:
@@ -127,9 +132,10 @@ class PTwikitextCleaner(object):
             elif type(node) == mwparserfromhell.nodes.Wikilink:
                 title = node.title.lower()
                 if any(link for link in wikilinks if link in title):
-                        remove_elements.append(node)
-                        
-        # Here the problem is the order of deletion or poorly structured wikitext due to malformation by the user or the manipulations performed by this code
+                    remove_elements.append(node)
+
+        # Here the problem is the order of deletion or poorly structured wikitext due
+        # to malformation by the user or the manipulations performed by this code
         for element in remove_elements:
             try:
                 prs.remove(element)
